ee22de290ba66a9406ab4e2c72bc5227f82269b6
David Johns <david.johns@icewire.ca>
2021-02-25 15:08:48 -0500

    updated buildExecutables.sh and .gitignore

--- buildExecutables.sh
+++ buildExecutables.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 # to run this shell script use: bash buildExecutables.sh
 #
-# Script is used to build and zip icemaker versions
-date=_200805
-env GOOS=windows GOARCH=amd64 go build -o icemaker.exe
-zip -r icemakerWin64$date.zip icemaker.exe
-rm icemaker.exe
-env GOOS=windows GOARCH=386 go build -o icemaker.exe
-zip -r icemakerWin32$date.zip icemaker.exe
-rm icemaker.exe
-env GOOS=linux GOARCH=amd64 go build -o icemaker
-zip -r icemakerLinux64$date.zip icemaker
-rm icemaker
-env GOOS=darwin GOARCH=amd64 go build -o icemaker
-zip -r icemakerMacOS64$date.zip icemaker
-rm icemaker
+# Script is used to build and zip binaries/problem2tex versions
+date=_210225
+env GOOS=windows GOARCH=amd64 go build -o binaries/problem2tex.exe
+zip -r binaries/problem2texWin64$date.zip binaries/problem2tex.exe
+rm binaries/problem2tex.exe
+env GOOS=linux GOARCH=amd64 go build -o binaries/problem2tex
+zip -r binaries/problem2texLinux64$date.zip binaries/problem2tex
+rm binaries/problem2tex
+env GOOS=darwin GOARCH=amd64 go build -o binaries/problem2tex
+zip -r binaries/problem2texMacOS64$date.zip binaries/problem2tex
+rm binaries/problem2tex
 
