1bf18fe198c3ac2c9158cdbafd9f20b92fdc3c99
David Johns <david.johns@icewire.ca>
2022-01-12 11:50:01 -0500

    delete files bugList.txt buildExecutables.sh runBackup.sh

--- runBackup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# shell script to backup this directory but ignore node_modules
-
-rsync -av --progress /Users/davidjohns/GitHub/go/src/problem2tex /Users/davidjohns/Dropbox/Icewire/git
