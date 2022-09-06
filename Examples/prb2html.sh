e36361376eace449c5c5cad6896a4390a96dbba9
David Johns <david.johns@icewire.ca>
2022-09-06 09:58:25 -0400

    fixed up examples

--- Examples/prb2html.sh
+++ Examples/prb2html.sh
@@ -2,7 +2,9 @@
 filename=$(basename -- "$1")
 extension="${filename##*.}"
 filename="${filename%.*}"
-cp prb_svgFix.js tmp/fix_svg_mathjax.js
-problem2tex -random=0 -outFlag=flagSolAns -export=tmp/$filename.org $filename.$extension
+cp prb_svgFix.js tmp/prb_svgFix.js
+cat prbHeader.txt $filename.$extension > temp235.prb
+problem2tex -random=0 -outFlag=flagSolAns -export=tmp/$filename.org temp235.prb
 emacs tmp/$filename.org --batch -f org-html-export-to-html --kill
+rm temp235.prb
 
