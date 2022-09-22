70483d49f1843e77a7206094ec0fc3f11eff534f
David Johns <david.johns@icewire.ca>
2022-09-22 10:41:21 -0400

    added test examples

--- runInkscape.sh
+++ runInkscape.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "export-latex; export-area-drawing;file-open:Examples/tmp/ex01NEWasc.svg; export-filename:Examples/tmp/ex01NEWasc.pdf; export-do; file-open:Examples/tmp/ex02NEW.svg; export-filename:Examples/tmp/ex02NEW.pdf; export-do; " | inkscape --shell
+echo "export-latex; export-area-drawing;file-open:Examples/tmp/nortonThev01NEWasc.svg; export-filename:Examples/tmp/nortonThev01NEWasc.pdf; export-do; " | inkscape --shell
